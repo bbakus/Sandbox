f33020caf41594d5cadde561e5d8f32c2debebb9
Brandon Bakus <brandonbakus@gmail.com>
2025-01-16 14:29:43 -0500

    reorganized , again, my JSON database to accomodate images for each card, refactored my drawCard and threeCardSpread to intake images and meanings from the database. Added HTML DIVs to hold cards. Plans to animate the cards somehow, from card bac k to face. So far everything is optimal and I'm making good progress

--- src/index.js
+++ src/index.js
@@ -1,81 +1,69 @@
 
 
-// const entireDeck = [
-//     fetch('http://localHost.3000/cardLibrary')
-//     .then(response => response.json())
-//     .then(cardLibrary => (){
 
-//     })
-//     redCards,
-//     orangeCards,
-//     yellowCards,
-//     greenCards,
-//     blueCards,
-//     purpleCards,
-//     pinkCards,
-//     whiteCards,
-//     blackCards,
-// ]
+
+
+
+
+const deckColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Pink', 'White', 'Black']
+
+const colorMeanings = {
+    Red: 'Red Cards: You are experiencing a primordial energy; so fiery and strong that it cannot be ignored. It asks you to pause and register these powerful feelings. How can you best deal with this influx of energy?',
+    Orange: 'Orange Cards: Tap into joy and playfulness right now. Orange is full of energy, which is invigorating but sometimes overstimulating. Are you excited, or is the energy too intense?',
+    Yellow: 'Yellow Cards: You may need to take a look at the duality of a situation. Yellow can be double-sided, are you seeing both sides? Are you limiting yourself to a singular viewpoint?',
+    Green: 'Green Cards: There is an opportunity for renewal, either through gratitude for abundance, movement on a new project, or rest. Let things unfold and grow.',
+    Blue: 'Blue Cards: Honor this time of transition and change. All blue cards deal with the crossing of a threshold. Surrender to the flow, and accept change.',
+    Purple: 'Purple Cards: You may be in the process of creating a deeper relationship with your intuition. Embrace this newfound energy, but stay grounded.',
+    Pink: 'Pink Cards: Tenderness and love surround you, either for yourself or another. Gentleness and sensitivity are your strengths right now; share them.',
+    White: 'White Cards: There is a sense of stillness for you to embrace. How can you manifest calm and tranquility in your life right now?',
+    Black: 'Black Cards: This is an invitation to step into a new phase of your life. How can you create space for newness, or cultivate courage?',
+}
+
 
 const entireDeck = [];
 
-('http://localhost:3000/cardLibrary')
-  .then(response => response.json())
-  .then(cardLibrary => {
-    // Add each color deck to entireDeck in order
-    entireDeck.push(cardLibrary.blackCards);
-    entireDeck.push(cardLibrary.whiteCards);
-    entireDeck.push(cardLibrary.pinkCards);
-    entireDeck.push(cardLibrary.purpleCards);
-    entireDeck.push(cardLibrary.blueCards);
-    entireDeck.push(cardLibrary.greenCards);
-    entireDeck.push(cardLibrary.yellowCards);
-    entireDeck.push(cardLibrary.orangeCards);
-    entireDeck.push(cardLibrary.redCards);
-    
-    // Now entireDeck is populated and you can use it
-    console.log(entireDeck);
-  })
+fetch('http://localHost:3000/cards')
+    .then(response => response.json())
+    .then(data => {
+        // Push each deck into entireDeck in the correct order
+        entireDeck.push(data.redCards);
+        entireDeck.push(data.orangeCards);
+        entireDeck.push(data.yellowCards);
+        entireDeck.push(data.greenCards);
+        entireDeck.push(data.blueCards);
+        entireDeck.push(data.purpleCards);
+        entireDeck.push(data.pinkCards);
+        entireDeck.push(data.whiteCards);
+        entireDeck.push(data.blackCards);
+    });
 
 
 function drawCard() {
-    // Pick a random color deck
+    
     const randomDeckIndex = Math.floor(Math.random() * entireDeck.length)
-    // If entireDeck has 4 decks (length of 4), Math.random() * 4 gives us 
-    // a number between 0-3.999..., then Math.floor rounds it down to 0, 1, 2, or 3
-
     const randomDeck = entireDeck[randomDeckIndex]
-    // Uses that random number to pick one of your color decks
-    // Like reaching into a shelf and picking either the black, blue, red, etc. deck
-    
-    // Get all card names from that color deck
     const cardNames = Object.keys(randomDeck)
-    // Object.keys gives us an array of all the card names
-    // If we picked the black deck, we'd get ["Death", "Fear", "Space", etc.]
-    
-    //Pick a random card from those names
     const randomCardIndex = Math.floor(Math.random() * cardNames.length)
-    // Same process as before - get a random index based on how many cards are in this deck
-    
     const randomCardName = cardNames[randomCardIndex]
-    // Use that random index to pick one of the card names
     
     return {
-        'Card': randomCardName.toUpperCase(),                // The name of the card
-        'Color': deckColors[randomDeckIndex],   // The color of the card
-        'Meaning': randomDeck[randomCardName]      // Look up this card's meaning
+        'Card': randomCardName.toUpperCase(),      
+        'Color': deckColors[randomDeckIndex],    
+        'Meaning': randomDeck[randomCardName].meaning,
+        'Image' : randomDeck[randomCardName].image  
     }    
     
 }
 
-let cardPullArray = [] // an empty array assigned to variable to contain the pulled cards for color meaning assesment
 
+
+let cardPullArray = [] 
 
 function colorCounts(){
-    let colorCounts = {} //create an empty object
-            for(let card of cardPullArray){ //iterate over cardPullArray
-                colorCounts[card.Color] = (colorCounts[card.Color] || 0) + 1 //card.Color gives color string from drawCard() return object, like "Black"
-            }  //the OR operator is simplifying an if statement: if the left side returns undefined, then we get 0) + 1
+    let colorCounts = {} 
+            for(let card of cardPullArray){ 
+                colorCounts[card.Color] = (colorCounts[card.Color] || 0) + 1 
+            }  
             for(let color in colorCounts){
                 if(colorCounts[color] > 1){
                     console.log(colorMeanings[color])
@@ -84,32 +72,76 @@ function colorCounts(){
 }
 
 
+// function threeCardSpreadWithDelay() {
+//     // let cardPullArray = [] 
+//     let firstCard = drawCard()
+//     console.log(firstCard)
+//     cardPullArray.push(firstCard) 
+
+//     setTimeout(() => {
+//         let secondCard = drawCard()
+//         if(secondCard === firstCard){ 
+//             secondCard = drawCard()
+//         }
+//         console.log(secondCard)
+//         cardPullArray.push(secondCard) 
+        
+//         setTimeout(() => {
+//             let thirdCard = drawCard()
+//             if(thirdCard === secondCard){
+//                 thirdCard = drawCard()
+//             }
+//             console.log(thirdCard)
+//             cardPullArray.push(thirdCard)  
+
+//             colorCounts()
+        
+//         }, 3000)
+//     }, 3000)
+// }
+
+
+
 function threeCardSpreadWithDelay() {
-    // let cardPullArray = [] 
-    let firstCard = drawCard()
-    console.log(firstCard)
-    cardPullArray.push(firstCard) 
+    let firstCard = drawCard();
+    cardPullArray.push(firstCard);
+    // Display first card
+    displayCard(firstCard, 1); // Helper function to handle DOM updates
 
     setTimeout(() => {
-        let secondCard = drawCard()
-        if(secondCard === firstCard){ // initially a while loop, but it didnt work, if seems to be running perfect
-            secondCard = drawCard()
+        let secondCard = drawCard();
+        if(secondCard === firstCard) {
+            secondCard = drawCard();
         }
-        console.log(secondCard)
-        cardPullArray.push(secondCard) 
-        
-        setTimeout(() => {
-            let thirdCard = drawCard()
-            if(thirdCard === secondCard){
-                thirdCard = drawCard()
-            }
-            console.log(thirdCard)
-            cardPullArray.push(thirdCard)  
+        cardPullArray.push(secondCard);
+        displayCard(secondCard, 2);
 
-            colorCounts()
-        
-        }, 3000)
-    }, 3000)
+        setTimeout(() => {
+            let thirdCard = drawCard();
+            if(thirdCard === secondCard) {
+                thirdCard = drawCard();
+            }
+            cardPullArray.push(thirdCard);
+            displayCard(thirdCard, 3);
+            colorCounts();
+        }, 3000);
+    }, 3000);
+}
+
+// Helper function to handle displaying each card
+function displayCard(card, position) {
+    const cardContainer = document.querySelector(`#card-${position}`);
+    const cardImage = cardContainer.querySelector('.card-image');
+    const cardMeaning = cardContainer.querySelector('.card-meaning');
+    const colorMeaning = cardContainer.querySelector('.color-meaning');
+
+    // Update DOM elements
+    cardImage.src = card.Image;
+    cardMeaning.textContent = card.Meaning;
+    colorMeaning.textContent = colorMeanings[card.Color];
+    
+    // Maybe add some animation classes
+    cardContainer.classList.add('card-reveal');
 }
 
 
@@ -117,15 +149,27 @@ function threeCardSpreadWithDelay() {
 
 function background(){
     const backgroundImage = document.createElement('img')
-    backgroundImage.src = 'Images/Others/3rd+Round+Edits_046.jpg'
+    backgroundImage.src = 'Images/Others/_BB18312.jpg'
     const html = document.querySelector('html')
     html.appendChild(backgroundImage)
     backgroundImage.id = 'night-sky'
 }
 
 function enterButton(){
-
+    const buttonElement = document.querySelector('#enter-button')
+    const enterTextElement = document.querySelector('#enter');
+    const prismTextElement = document.querySelector('.tessellation-container');
+    enterTextElement.style.cursor = 'pointer'
+    buttonElement.addEventListener('click', () => {
+        console.log('FUCK')
+        prismTextElement.style.display = "none";
+        enterTextElement.style.display = "none";
+        buttonElement.style.display = "none"
+        // landingCardSpread()
+    })
 }
+
+
 
 
 
@@ -133,7 +177,7 @@ addEventListener("DOMContentLoaded", (event) => {
 
 
 
-
+    enterButton()
     background()
 
 
