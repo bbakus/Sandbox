b5f850bb91549ed3f87be5c139ad28c95f06420f
Brandon Bakus <brandonbakus@gmail.com>
2025-01-23 14:58:42 -0500

    trying the touch event handler, click does not work

--- src/index.js
+++ src/index.js
@@ -153,7 +153,7 @@ function threeCardSpread() {
 
 
     setTimeout( () => {
-        cardOneReveal.addEventListener("click", () =>{  
+        cardOneReveal.addEventListener("touch", () =>{  
             
             firstCard = drawCard()
             cardPullArray.push(firstCard)
@@ -162,7 +162,7 @@ function threeCardSpread() {
             handleCardVanish('firstCard') 
 
         }, {once: true})
-        cardTwoReveal.addEventListener("click", () => {
+        cardTwoReveal.addEventListener("touch", () => {
 
             secondCard = drawCard()
             while (cardPullArray.some(card => card.Card === secondCard.Card)) {
@@ -173,7 +173,7 @@ function threeCardSpread() {
             displayCard(secondCard, 2)
 
         }, {once: true})
-        cardThreeReveal.addEventListener("click", () => {
+        cardThreeReveal.addEventListener("touch", () => {
         
             thirdCard = drawCard()
             while (cardPullArray.some(card => card.Card === thirdCard.Card)) {
